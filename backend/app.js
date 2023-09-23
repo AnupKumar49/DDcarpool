23c03063576c7967544b8e8e196a31d2fe603f45
sahilmangla1901 <sahilmangla1901@gmail.com>
2023-09-23 12:50:59 +0530

    initial part added

---
+++ backend/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyparser = require('body-parser');
+const cookieparser = require('cookie-parser');
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config()
+const { Client } = require("@googlemaps/google-maps-services-js");
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
+
+const authRoutes = require("./Routes/authentication");
+const allusersRoutes = require("./Routes/allusersRoutes");
+//const userRoutes = require("./Routes/user.js");
+const tripRoutes = require("./Routes/tripRoutes");
+
+// import cookieparser from "cookie-parser";
+// import cors from "cors";
+//import swaggerUI from "swagger-ui-express";
+//import YAML from 'yamljs';
+//import dotenv from "dotenv" 
+// import authRoutes from "./Routes/authentication.js";
+// import userRoutes from "./Routes/user.js";
+// import allusersRoutes from "./Routes/allusersRoutes.js";
+//const specs = swaggerJsDoc(options);
+//Middleware
+
+//PORT
+
+
+// MongoDb connection
+var db=mongoose.connect('mongodb+srv://sahilmangla1901:xP3VIyABshxHhDJ9@cluster0.q1v6y4u.mongodb.net/Carpool').then(console.log("DB connected"))
+//.catch(error => console.log(error));
+
+//Middleware
+app.use(bodyparser.json())
+app.use(cookieparser())
+app.use(cors())
+
+//Routes
+app.use("/api", authRoutes);
+app.use("/api", allusersRoutes);
+//app.use("/api", userRoutes);
+app.use("/api", tripRoutes);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+const server = app.listen(process.env.PORT || 8000, 'localhost', () => {
+    const address = server.address();
+    console.log(`Server is running on http://${address.address}:${address.port}`);
+  });
+
+
+
+    
+module.exports = app;
+// MongoDb connection
